08c80c698832d282f883cebef61b5e316df835dc
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-06-23 21:08:37 -0300

    [fix] erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 
 const app = express();
 
@@ -9,5 +11,17 @@ app.use(express.json());
 import {router} from "./routes/routes";
 
 app.use(router);
+
+app.use ((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => console.log('Server is running'));
